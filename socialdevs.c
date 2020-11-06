c26683aae13ab41dd0d7123d6760a76221855038
Nathalia <nathaliacarnevalli444@gmail.com>
2020-11-06 19:52:46 -0300

    1 commit

--- socialdevs.c
+++ socialdevs.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,17 +10,17 @@ struct data_devs
     char password[50];
     int id;
     char bio[100];
+    int status; 
 };
-
 
 void create(struct data_devs *d, int *code)
 {
-    char t; //sÃ³ pra definir uma tecla de atalho
+    char t; //são pra definir uma tecla de atalho
 
     system("clear");
     printf("\n--- CADASTRO DE DEVS ---\n\n");
 
-    (*code)++; //pega a posiÃ§Ã£o do dev
+    (*code)++; //pega a posição do dev
 
     fflush(stdin);
     setbuf(stdin,NULL);
@@ -52,13 +51,13 @@ void create(struct data_devs *d, int *code)
     setbuf(stdin,NULL);
 
 
-    d[*code].id=(*code)+1; //soma 1 pra nÃ£o pegar o mesmo numero anterior
+    d[*code].id=(*code)+1; //soma 1 pra não pegar o mesmo numero anterior
 
     printf("\n\nCadastro realizado com sucesso.\nAperte enter ou qualquer tecla seguida por enter para retornar ao menu.\n");
     fflush(stdin);
     setbuf(stdin,NULL);
 
-    scanf("%c", &t); //le a tecla que o usuÃ¡rio digitar
+    scanf("%c", &t); //le a tecla que o usuário digitar
     system("clear");
 }
 
@@ -76,13 +75,13 @@ void delete(struct data_devs *d, int *code)
     scanf("%d", &idRemove);
 
     if (*code>=0){ //se existir devs cadastrados
-        for (i=0; i<=*code; i++) //percorre o vetor de devs para achar o cÃ³digo
+        for (i=0; i<=*code; i++) //percorre o vetor de devs para achar o código
         {
-            if(d[i].id == idRemove){  //se o cÃ³digo da vez que estiver no for, for igual ao cÃ³digo que o usuÃ¡rio digitou
+            if(d[i].id == idRemove){  //se o cÃ³digo da vez que estiver no for, for igual ao código que o usuário digitou
                 for (j = i; j < *code; j++)
                 {
                     d[j] = d[j+1];
-                    d[j].id=d[j+1].id-1;  //serve para atualizar o codigo dos devs apÃ³s um ser removido, para nÃ£o dar problema com o cadastro de novos devs depois
+                    d[j].id=d[j+1].id-1;  //serve para atualizar o codigo dos devs opções um ser removido, para nÃ£o dar problema com o cadastro de novos devs depois
                 }
 
                 (*code)--; //pra ir diminuindo no for
@@ -95,7 +94,130 @@ void delete(struct data_devs *d, int *code)
     scanf("%c", &t);
 
     system("clear");
+}
+
+void list(struct data_devs *d, int *code){
+	
+	int i,cod,n=0;
+	char t;
+	system("clear");
+    printf("\n--- LISTAGEM DE DEVS CADASTRADOS ---\n\n");
+    
+    for(i=0; i<= *code; i++)
+	{
+    	printf("\n\tNome: %s\n\Usuário: %s\n\tEmail: %s\n\tBio: %s\n", d[i].name, d[i].user, d[i].id, d[i].email, d[i].password, d[i].bio);
+    	 if(d[i].status==0)
+		 {
+    	 	printf("Status: Ativo\n");
+		 }else{
+		 	printf("Status: Offline\n");
+		 }
+		 printf("Para saber o id e a senha de cada usuário digite o codigo de acesso: "); //codigo de acesso é 1234.
+		 scanf("%i", &cod);
+		 n=1;
+    	 if(cod==1234)
+		 {
+    	 	printf("\n\tID: %i\n\tPassword: %s", d[i].id, d[i].password);
+		 }
+	}
+	     if(!n)
+		 {
+	     	printf("Codigo de acesso invalido.");
+		 }
+	printf("\n\nAperte enter ou qualquer tecla seguida por enter para retornar ao menu.\n");
+    fflush(stdin);
+    setbuf(stdin,NULL);
+    scanf("%c",&t);
+	system("clear");
+	
+}
+
+void consult(struct data_devs *d, int *code)
+{
+    int i, ids, resp, n=0;
+    char t, dev[50];
+
     system("CLS");
+    printf("\n--- CONSULTA ---\n\n");
+    printf("\nDeseja consultar pelo id ou pelo nome de usuário?\n");
+    printf("\nDigite:\n1- Id.\n2- Usuário\n");
+    scanf("%d", &resp);
+    switch(resp){
+    case 1: //caso escolha consultar pelo id
+        system("clear");
+        printf("\nInsira o id do DEV a ser consultado:\n\n");
+        scanf("%d", &ids);
+        for(i=0; i <= *code; i++) //percorre o vetor de devs
+        {
+            if(ids == d[i].id) 
+            {   
+                printf("\n\tNome: %s\n\Usuário: %s\n\tID: %d\n\tEmail: %s\n\tPassword: %i\n\tBio:\n\t", d[i].name, d[i].user, d[i].id, d[i].email, d[i].password, d[i].bio);
+                n=1; 
+                if(d[i].status==0) 
+                {
+                    printf("Status: Online");
+                }
+                else if (d[i].status==1)
+                {
+                    printf("Status: Offline");
+                }
+            }
+	    }
+	    if(!n){
+        	printf("ID inexistente.\n");
+			
+		}
+       
+        printf("\n\nAperte enter ou qualquer tecla seguida por enter para retornar ao menu.\n");
+        fflush(stdin);
+        setbuf(stdin,NULL);
+        scanf("%c", &t);
+        system("clear");
+    break;
+
+    case 2: //caso escolha consultar pelo usuário;
+        system("clear");
+        printf("\nInsira o nome do usuário apenas em letras minusculas: \n\n");
+        fflush(stdin);
+        setbuf(stdin,NULL);
+        fgets(dev, 50, stdin);
+        for(i=0; i <= *code; i++) //percorre o vetor de devs
+        {
+            if(strcmp(dev, d[i].user) == 0) 
+            {
+            	n=1;
+                printf("\n\tNome: %s\n\Usuário: %s\n\tID: %d\n\tEmail: %s\n\tPassword: %i\n\tBio:\n\t", d[i].name, d[i].user, d[i].id, d[i].email, d[i].password, d[i].bio);
+                if(d[i].status == 0) 
+                {
+                    printf("Status: Online");
+                }
+                else if (d[i].status == 1)
+                {
+                    printf("Status: Offline");
+                }
+            }
+        }
+        if (!n){ 
+            printf("Usuário inexistente.\n");
+        }
+        printf("\n\nAperte enter ou qualquer tecla seguida por enter para retornar ao menu.\n");
+        fflush(stdin);
+        setbuf(stdin,NULL);
+        scanf("%c", &t);
+        system("clear");
+    break;
+
+    default://caso não escolha nenhuma das opções. 
+        system("CLS");
+        printf("\nOpcao invalida.\n");
+        printf("\n\nAperte enter ou qualquer tecla seguida por enter para retornar ao menu.\n");
+        fflush(stdin);
+        setbuf(stdin,NULL);
+        scanf("%c", &t);
+        system("clear");
+    break;
+    }
+	
 }
 
 
@@ -110,8 +232,8 @@ int main(){
 
     if (resp == 1) //1 = Acessa menu
     {
-        system("CLS");
-        while(resp != 0) //Enquanto o usuÃ¡rio nÃ£o quiser sair, pois em todo final de processo/funÃ§Ã£o, ele pode clicar em sair.
+        system("clear");
+        while(resp != 0) //Enquanto o usuário não quiser sair, pois em todo final de processo/função, ele pode clicar em sair.
         {
             printf("\n---- MENU ----\n\n");
             resp = -1;
@@ -129,14 +251,12 @@ int main(){
                 case 2:
                     delete(devs, &code); //mesma coisa que o de cima
                 break;
-                /*
                 case 3:
-                    listagem(livro, &cont);
+                    list(devs, &code);
                 break;
                 case 4:
-                    consulta(livro, &cont);
+                    consult(devs, &code);
                 break;
-                */
                 default:
                     printf("\nProcesso finalizado.\n");
                 break;
